a64515226ccb75958b6d9a6c44d3879564445643
Rutuja Dudhagundi <rutuja.dudhagundi@ibm.com>
2021-04-20 13:40:32 +0530

    Updated Abc.java and created Xyz.java

--- Abc.java
+++ Abc.java
@@ -2,6 +2,10 @@ public class Abc
 {
 	public static void main(String args[])
 	{
-		System.out.println("HELLO WORLD");
+		for(int i=0; i<10; i++)
+		{
+			System.out.println("HELLO WORLD");
+	
+		}
 	}
 }
---
+++ Xyz.java
@@ -0,0 +1,7 @@
+public class Xyz
+{
+	public void display()
+	{
+		System.out.println("From XYZ class");
+	}
+}
